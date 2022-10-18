1cd1cea7ce1940cd10c342e73e876b8150db4333
EwelinaManiecka <ewelina.maniecka@op.pl>
2022-10-18 15:32:29 +0200

    Update fetchCountries.js
    fetch filtr - only needs information

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -4,7 +4,7 @@ import {Notify} from "notiflix/build/notiflix-notify-aio";
 const START_URL = "https://restcountries.com/v3.1";
 
 export function fetchCountries(name) {
-    return fetch(`${START_URL}/name/${name}`)
+    return fetch(`${START_URL}/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response => {
         if (!response.ok) {
             return Notify.failure("Oops, there is no country with that name");
